1fb751d1b734f94637c308d17c65c3c8f6434f7f
IBartishvili <iona.bartishvili@gmail.com>
2022-01-16 20:54:57 +0400

    initial commit

---
+++ app/src/main/java/dev/iona/github_actions_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.iona.github_actions_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
